49ff26ff14b74ff6c2e78ed34e2d8d9fdeeaa153
lguiller <lguiller@student.42.fr>
2019-11-04 11:55:58 +0100

    ajout de la suite du programme, calcule de a b c delta et des solutions, pour l'instant tout fonctionne correctement

--- test.py
+++ test.py
@@ -2,8 +2,9 @@
 
 import sys
 import re
+from math import sqrt
 
-def reduce_equation(equation):
+def remove_whitespaces(equation):
     equation = sys.argv[1].strip()
     equation = equation.replace(' ', '')
     equation = equation.lower()
@@ -16,7 +17,7 @@ def split_elem(equation, chars):
     array = []
     i = 0
     for j in range(0, len(equation)):
-        if (chars.find(equation[j]) != -1):
+        if (chars.find(equation[j]) != -1 and j != 0):
             array.append(equation[i:j])
             i = j
     array.append(equation[i:])
@@ -24,8 +25,8 @@ def split_elem(equation, chars):
 
 def split_into_array(equation):
     array = []
-    array.append(split_elem(equation[0], '+-'))
-    array.append(split_elem(equation[1], '+-'))
+    for elem in equation:
+        array.append(split_elem(elem, '+-'))
     return (array)
 
 def mul(a, b):
@@ -38,30 +39,62 @@ def make_calc(calc):
     calc_functs = [mul, div]
     calc_chars = ['*', '/']
     array = re.split('([/\*])', calc)
-    res = 0
-
-    print(array)
+    for i in range(0, len(array)):
+        if (array[i] == '-'):
+            array[i] = -1.0
+        elif (array[i] == '' or array[i] == '+'):
+            array[i] = 1.0
+    res = 0.0
+    i = 0
+    while (len(array) != 1):
+        for char, funct in zip(calc_chars, calc_functs):
+            if (array[i] == char):
+                res = funct(array[i - 1], array[i + 1])
+                del array[i - 1:i + 2]
+                array.insert(0, res)
+                i = 0
+        i += 1
+    res = float(array[0])
+    return (res)
 
 def reduce_elem(equation):
     a = 0
     b = 0
     c = 0
-    for elem in equation[0]:
-        if (elem.find('x^2') != -1):
-            value = make_calc(elem.replace('x^2', ''))
-            print(value)
+    for i in range(0, len(equation)):
+        sign = 1.0 if (i == 0) else -1.0
+        for elem in equation[i]:
+            if (elem.find('x^2') != -1):
+                a += sign * make_calc(elem.replace('x^2', ''))
+            elif (elem.find('x') != -1):
+                b += sign * make_calc(elem.replace('x', ''))
+            else:
+                c += sign * make_calc(elem)
+    return (a, b, c)
 
 def main():
     if (len(sys.argv) != 2):
         print("usage: ./computorV1 [equation]")
         sys.exit(1)
-    equation = reduce_equation(sys.argv[1])
+    equation = remove_whitespaces(sys.argv[1])
     print(equation)
     equation = split_equal(equation)
     print(equation)
     equation = split_into_array(equation)
     print(equation)
-    reduce_elem(equation)
+    a, b, c = reduce_elem(equation)
+    print('{}x^2 + {}x + {}'.format(a, b, c))
+    delta = b ** 2 - 4 * a * c
+    print('delta = {}'.format(delta))
+    if (delta > 0):
+        x1 = (-b - sqrt(delta)) / (2 * a)
+        x2 = (-b + sqrt(delta)) / (2 * a)
+        print('x1 = {}\nx2 = {}'.format(x1, x2))
+    elif (delta == 0):
+        x = -b / (2 * a)
+        print('x = {}'.format(x))
+    else:
+        print('pas de solution reel')
 
 if __name__ == '__main__':
     main()
